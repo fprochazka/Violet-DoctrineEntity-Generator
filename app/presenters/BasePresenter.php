eea53f84f31d96dbb6f0c4d9273c880c15007790
Filip Procházka <hosiplan@gmail.com>
2011-08-03 23:25:04 +0200

    typo

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -2,9 +2,6 @@
 
 /**
  * Base class for all application presenters.
- *
- * @author     John Doe
- * @package    MyApplication
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
